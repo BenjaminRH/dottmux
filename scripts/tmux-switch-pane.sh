1c3894282f4c57d08f540e1fcc5394d6bb1f5f2d
BenjaminRH <benjamin3harris@gmail.com>
2022-04-26 22:34:10 +0300

    Fuzzy finder

---
+++ scripts/tmux-switch-pane.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Configure
+LIST_DATA="#{window_name} [#{pane_index}] #{pane_current_path}"
+FZF_COMMAND="fzf-tmux -p -- --delimiter=: --with-nth 4 --color=hl:2"
+
+# Do not change
+TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
+
+# Select a pane
+LINE=$(tmux list-panes -a -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
+# Split the result
+args=(${LINE//:/ })
+# Activate the session/window/pane
+tmux display-message "ARGS = ${args[0]} ${args[1]} ${args[2]}"
+tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
