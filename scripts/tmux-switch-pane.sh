edb5a708c151b34aaa3e1891cb5d51c82ca4bf5d
BenjaminRH <benjamin3harris@gmail.com>
2022-04-28 12:52:51 +0300

    Temporarily disable stuff

--- scripts/tmux-switch-pane.sh
+++ scripts/tmux-switch-pane.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Configure
-LIST_DATA="#{window_name} [#{pane_index}]>#{pane_current_path} | #{pane_title}"
-#FZF_COMMAND="fzf-tmux -p -- --delimiter=: --with-nth 4 --color=hl:2"
-FZF_COMMAND="fzf-tmux -- --delimiter=: --with-nth 4 --color=hl:2"
+#LIST_DATA="#{window_name} [#{pane_index}]>#{pane_current_path} | #{pane_title}"
+LIST_DATA="#{window_name} [#{pane_index}] @ #{pane_current_path}"
+FZF_COMMAND="fzf-tmux -p -- --delimiter=: --with-nth 4 --color=hl:2"
+#FZF_COMMAND="fzf-tmux -- --delimiter=: --with-nth 4 --color=hl:2"
 
 # Do not change
 TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
